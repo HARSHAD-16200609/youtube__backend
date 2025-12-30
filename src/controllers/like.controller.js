74b7ae1012bf960f215eea92ffd3a9f18b65a26b
harshad diwate <harshaddiwate85@gmail.com>
2025-12-30 14:48:20 +0530

    fix: correct video like toggle functionality and improve error handling

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,49 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {API_Error} from "../utils/Api_error.js"
-import {Api_Response} from "../utils/Api_Response.js"
-import {async_handler} from "../utils/async-handler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { API_Error } from "../utils/Api_error.js";
+import { Api_Response } from "../utils/Api_Response.js";
+import { async_handler } from "../utils/async-handler.js";
 
 const toggleVideoLike = async_handler(async (req, res) => {
-    const {videoId} = req.params
+    const { videoId } = req.params;
+    const userId = req.user._id;
     //TODO: toggle like on video
-})
+    if (!isValidObjectId) throw new API_Error(400, "Enter an valid VideoId");
+
+    const existingLike = await Like.findOne({
+        video: videoId,
+        likedBy: userId,
+        
+    });
+    if (existingLike){
+        await existingLike.deleteOne();
+        return res.status(200)
+            .json(new Api_Response(200,{}, "Unliked the Video Successfully"));
+    } 
+    else {
+        const like = await Like.create({
+            video: new mongoose.Types.ObjectId(videoId),
+            likedBy: userId,
+           
+        });
+        if (!like) throw new API_Error(500, "Failed to like the video");
+
+        return res
+            .status(200)
+            .json(new Api_Response(200, like, "Liked the Video Successfully"));
+    }
+});
 
 const toggleCommentLike = async_handler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
 
-})
+});
 
 const toggleTweetLike = async_handler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+
+});
 
 const getLikedVideos = async_handler(async (req, res) => {
-    //TODO: get all liked videos
-})
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+});
+
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
